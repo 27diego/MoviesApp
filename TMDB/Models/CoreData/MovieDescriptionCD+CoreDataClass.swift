b1a99a31e78ef1f54a2903e7bb56ec5d0b232a63
Developer <developer@Developers-iMac.local>
2021-01-15 14:02:27 -0800

    movie description is ready to be decoded, need to set up remaining relationships like genre in xcdatamodeld

--- TMDB/Models/CoreData/MovieDescriptionCD+CoreDataClass.swift
+++ TMDB/Models/CoreData/MovieDescriptionCD+CoreDataClass.swift
@@ -10,8 +10,40 @@ import Foundation
 import CoreData
 
 @objc(MovieDescriptionCD)
-public class MovieDescriptionCD: NSManagedObject {
+public class MovieDescriptionCD: NSManagedObject, Codable {
+    enum MovieDescriptionCodingKeys: String, CodingKey {
+        case backdropPath, identifier="id", overview, popularity, posterPath, releaseDate, runtime, title
+    }
+    
+    public required convenience init(from decoder: Decoder) throws {
+        guard let context = decoder.userInfo[.managedObjectContext] as? NSManagedObjectContext else {
+            fatalError("Did not provide correct context")
+        }
+        self.init(context: context)
+        
+        let container = try decoder.container(keyedBy: MovieDescriptionCodingKeys.self)
+        self.backdropPath = try container.decode(String.self, forKey: .backdropPath)
+        self.identifier = try container.decode(Int.self, forKey: .identifier)
+        self.overview = try container.decode(String.self, forKey: .overview)
+        self.popularity = try container.decode(Double.self, forKey: .popularity)
+        self.posterPath = try container.decode(String.self, forKey: .posterPath)
+        self.releaseDate = try container.decode(String.self, forKey: .releaseDate)
+        self.runtime = try container.decode(Int.self, forKey: .runtime)
+        self.title = try container.decode(String.self, forKey: .title)
+    }
 
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: MovieDescriptionCodingKeys.self)
+        try container.encode(backdropPath, forKey: .backdropPath)
+        try container.encode(identifier, forKey: .identifier)
+        try container.encode(overview, forKey: .overview)
+        try container.encode(popularity, forKey: .popularity)
+        try container.encode(posterPath, forKey: .posterPath)
+        try container.encode(releaseDate, forKey: .releaseDate)
+        try container.encode(runtime, forKey: .runtime)
+        try container.encode(title, forKey: .title)
+    }
+    
 }
 
 
@@ -22,12 +54,12 @@ extension MovieDescriptionCD {
     }
 
     @NSManaged public var backdropPath: String?
-    @NSManaged public var id: Int64
+    @NSManaged public var identifier: Int
     @NSManaged public var overview: String?
     @NSManaged public var popularity: Double
     @NSManaged public var posterPath: String?
     @NSManaged public var releaseDate: String?
-    @NSManaged public var runtime: Int64
+    @NSManaged public var runtime: Int
     @NSManaged public var title: String?
     @NSManaged public var lastUpdated: String?
     @NSManaged public var actors: NSSet?
